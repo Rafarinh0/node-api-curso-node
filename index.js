e971b43c4fd7a88fc24405d4923ef15fda23a564
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-05-24 16:42:57 -0300

    Primeira rota criada, explicação res e req

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req,res)=>{
+    res.send('Hello World');
+});
+//primeiro parametro é o que vem depois da barra lá no localhost
+//o segundo é uma função que recebe dois parâmetros: req e res
+//o req simboliza a requisição que eu estou fazendo ao servidor, e vai conter detalhes dessa informação
+//o res tem a ver com a resposta que eu vou dar para a requisição
+//back-end, no geral, é fazer uma requisição e o servidor devolvendo uma resposta
 app.listen(3001);
