fed9f9d04c02a328a9ee0b232ffa28d8218827c2
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-05-25 17:29:04 -0300

    Conexão ao MongoDB com o Mongoose + teste model de produto

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+//Iniciando o app
 const app = express();
 
+// Iniciando o Banco de Dados
+mongoose.connect('mongodb://192.168.99.100:27017:27017/nodeapi', 
+{useNewUrlParser: true});
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+// Primeira Rota
 app.get('/', (req,res)=>{
-    res.send('Hello Rocketseat');
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    });
+
+   return res.send('Hello Rocketseat!');
 });
 //primeiro parametro é o que vem depois da barra lá no localhost
 //o segundo é uma função que recebe dois parâmetros: req e res
