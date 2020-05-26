4214c5d053982e055d1cb01a68caf5d04ea7a780
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-05-26 16:00:44 -0300

    +Rotas e métodos async de detalhamento dos produtos, atualização e remoção

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
 module.exports = {
+    // Rota de Listagem  
     async index(req, res){
         const products = await Product.find();
         // o retorno só vai ocorrer depois que todos os registros do banco de dados
@@ -10,10 +11,35 @@ module.exports = {
         return res.json(products);
     },
 
+    //Rota de Detalhes
+    async show(req,res){
+        const product = await Product.findById(req.params.id);
+
+        return res.json(product);
+    },
+
+    // Rota de Criação
     async store(req, res){
         // Criação de produtos
         const product = await Product.create(req.body);//Passa como parâmetro todos os campos que
                                                        //estão no corpo da requisição
         return res.json(product);
+    },
+
+    // Rota de Atualização
+    async update(req, res){
+        const product = await Product.findByIdAndUpdate(req.params.id,req.body, {new: true})
+        // Vai encontrar um produto pelo id dele e atualizar ele com o conteúdo que vem do req.body
+
+        //O "new: true" pede que o mongoose retorne o produto atualizado pra variável product
+        //Caso contrário, a função vai retornar pro product o produto antes de atualizar o req.body
+        return res.json(product);
+    },
+
+    async destroy(req, res){
+        await Product.findByIdAndRemove(req.params.id);
+
+        return res.send();
+        //só manda uma mensagem de sucesso mesmo
     }
 };
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,10 @@ const routes = express.Router();
 const ProductController = require('./controllers/ProductController');
 
 routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
 routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
 
 //O post é usado quando eu chamo uma rota que vai criar alguma coisa
 module.exports = routes;
